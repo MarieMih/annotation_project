a81cf065459689b81b323fb5853746f8ad6bd371
Mariya Mikhailycheva <mmikhailycheva@pyxis.ripcm.com>
2024-09-06 16:59:09 +0300

    structure

--- downloading.py
+++ downloading.py
@@ -1,4 +1,11 @@
 import requests
+import sys
+"""
+For downloading proteins from Uniprot, but actually
+from elsewhere.
+First argument of cmd is a name to record, 
+second argument is a formed api query from Uniprot.
+"""
 
 def download_uniprot_data(api_url, output_file):
     try:
@@ -13,7 +20,7 @@ def download_uniprot_data(api_url, output_file):
         print(f'Other error occurred: {err}')
 
 if __name__ == "__main__":
-    api_url = 'https://rest.uniprot.org/uniprotkb/stream?compressed=true&format=fasta&query=%28%28taxonomy_name%3A%22Escherichia+phage%22%29%29+AND+%28reviewed%3Afalse%29'
-    output_file = 'uniprotkb_escherichia_phage.txt'
+    output_file = sys.argv[1]
+    api_url = sys.argv[2]
     download_uniprot_data(api_url, output_file)
 
