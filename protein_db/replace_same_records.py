f1d08f9c791c4549b878b2c2dfcf2a8752f13dbc
MarieMih <maria.mihailicheva@yandex.ru>
2024-09-18 16:53:55 +0300

    good version of replace_same_records.py

--- protein_db/replace_same_records.py
+++ protein_db/replace_same_records.py
@@ -3,26 +3,20 @@ from Bio import SeqIO
 
 
 def replace_unique_fasta(input_file, output_file):
-    all_records = []
-    unique_headers = []
-    header_keys = []
-
-    # Parse the FASTA file and replace unique headers
-    for record in SeqIO.parse(input_file, "fasta"):
-        header_key = " ".join(record.description.split(" ")[1:])
-        if header_key not in header_keys:
-            all_records.append(record)
-            unique_headers.append(record.description)
-            header_keys.append(header_key)
-        else:
-            new_header = [i for i in unique_headers if header_key in i]
-            assert new_header[0] != ''
-            record.description = new_header[0]
-            record.id = new_header[0].split(' ')[0]
-            all_records.append(record)
+    unique_headers = {}
 
     with open(output_file, "w") as output_handle:
-        SeqIO.write(all_records, output_handle, "fasta")
+        for record in SeqIO.parse(input_file, "fasta"):
+            header_key = " ".join(record.description.split(" ")[1:])
+
+            if header_key not in unique_headers:
+                unique_headers[header_key] = (record.description, record.id)
+                SeqIO.write(record, output_handle, "fasta")
+            else:
+                existing_description, existing_id = unique_headers[header_key]
+                record.description = existing_description
+                record.id = existing_id
+                SeqIO.write(record, output_handle, "fasta")
 
 
 input_file = sys.argv[1]
