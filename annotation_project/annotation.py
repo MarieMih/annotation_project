9199797917add59a56095e74a1e59a14aac4e247
MarieMih <maria.mihailicheva@yandex.ru>
2025-06-09 14:21:12 +0300

    fix removed import

--- annotation_project/annotation.py
+++ annotation_project/annotation.py
@@ -10,7 +10,6 @@ sys.path.append(os.path.dirname(__file__))
 from divide_tsv import divide_tsv
 from divide_fasta import divide_fasta
 from extract_uniref import extract_uniref
-from ref2kb import converting_uniref_to_uniprotkb
 from catch_ids import catch_ids
 from correcting_gff import correcting_gff
 from converting_to_gtf import convert_gff_to_gtf
